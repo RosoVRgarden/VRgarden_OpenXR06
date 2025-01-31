56e9540c4118fa9d15694012e9e777b31a8c3283
Nicolas Marechal <68047863+RosoVRgarden@users.noreply.github.com>
2025-01-31 18:47:39 +0800

    VRgarden2025

--- Assets/Quixel/Scripts/BridgeImporter/MegascansMatertialUtils.cs
+++ Assets/Quixel/Scripts/BridgeImporter/MegascansMatertialUtils.cs
@@ -10,40 +10,22 @@ namespace Quixel
     {
         public static Material CreateMaterial(int shaderType, string matPath, bool isAlembic, int dispType, int texPack)
         {
-            if ((shaderType == 0 || shaderType == 1) && isAlembic)
-            {
-                Debug.Log("Alembic files are not supported in LWRP/HDRP. Please change your export file format in Bridge or change your SRP in Unity.");
-                return null;
-            }
-
             try
             {
                 string rp = matPath + ".mat";
                 Material mat = (Material)AssetDatabase.LoadAssetAtPath(rp, typeof(Material));
+
                 if (!mat)
                 {
-                    mat = new Material(Shader.Find("Standard"));
+                    mat = new Material(Shader.Find("Universal Render Pipeline/Lit"));
                     AssetDatabase.CreateAsset(mat, rp);
                     AssetDatabase.Refresh();
+                
                     if (shaderType < 1)
                     {
                         mat.shader = Shader.Find("Universal Render Pipeline/Lit");
                     }
-                    if (shaderType > 1)
-                    {
-                        if (isAlembic)
-                        {
-                            mat.shader = Shader.Find("Alembic/Standard");
-                            if (texPack > 0)
-                                mat.shader = Shader.Find("Alembic/Standard (Specular setup)");
-                        }
-                        else
-                        {
-                            mat.shader = Shader.Find("Standard");
-                            if (texPack > 0)
-                                mat.shader = Shader.Find("Standard (Specular setup)");
-                        }
-                    }
+                    
                 }
                 return mat;
             }
